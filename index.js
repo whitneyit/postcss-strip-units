d4a2c0e2daa930c5fdd551c0373dd6eecfc5f488
Michael Schlotfeldt <michael@plaudit.com>
2017-12-03 17:37:38 -0500

    Upgraded to PostCSS 6, added "functionName" option, etc. (#5)
    * Updated eslintrc; tried to keep same rules with upgrade and add es6 support.
    
    * Documented new option
    
    * Adeded yarn lock
    
    * Added more tests and upgrade test to work with new versino of postcss.
    
    * Upgraded dependencies, merged code during updated, added warnings using new PostCSS's warn method, etc.
    
    * Fixed so warnings are printed all on one line.

--- index.js
+++ index.js
@@ -1,26 +1,62 @@
-var postcss = require('postcss');
-var helpers = require('postcss-message-helpers');
-var transformStrip = require('./lib/transformStrip');
+const postcss = require('postcss');
+const reduceFunctionCall = require('reduce-function-call');
 
-module.exports = postcss.plugin('postcss-strip', function () {
-    return function (css) {
-        function transformValue(node, property) {
-            var value = node[property];
+const PLUGIN_NAME = 'postcss-strip';
 
-            if (!value || value.indexOf('strip(') === -1) {
-                return;
+function stripTransform(node, result, argString, functionIdentifier) {
+    argString = argString.trim();
+    var args = argString.split(','),
+        rawValue,
+        parsedValue;
+
+    switch (args.length) {
+        case 0:
+            result.warn(`Not enough arguments passed "${functionIdentifier}()". Expected one. Did not receive any.`, { node: node });
+            break;
+        case 1:
+            rawValue = args[0];
+            if (rawValue === '') {
+                result.warn(`No valid arguments passed "${functionIdentifier}()". Expected one. Got "${argString}"`, { node: node });
             }
+            break;
+        default:
+            result.warn(`Too many args passed to "${functionIdentifier}()". Expected one. Got "${argString}"`, { node: node });
+    }
 
-            helpers.try(function transformStripValue () {
-                node[property] = transformStrip(value);
-            }, node.source);
-        }
+    parsedValue = parseFloat(rawValue);
 
+    if (isNaN(parsedValue)) {
+        result.warn(`"${functionIdentifier}(${argString})" resulted in "NaN". Left original value in place.`, { node: node });
+        return argString;
+    }
+
+    return parsedValue;
+}
+
+
+function transformValue(node, property, functionIdentifier, boundStripTransform) {
+    var value = node[property];
+    if (value === undefined || value.indexOf(functionIdentifier + '(') === -1) {
+        return;
+    }
+    node[property] = reduceFunctionCall(value, functionIdentifier, boundStripTransform);
+}
+
+module.exports = postcss.plugin(PLUGIN_NAME, function (opts) {
+    opts = Object.assign(
+        {
+            functionName : 'strip'
+        },
+        opts
+    );
+
+    return function (css, result) {
         css.walk(function (node) {
+            const boundStripTransform = stripTransform.bind(null, node, result);
             if (node.type === 'atrule') {
-                return transformValue(node, 'params');
+                return transformValue(node, 'params', opts.functionName, boundStripTransform);
             } else if (node.type === 'decl') {
-                return transformValue(node, 'value');
+                return transformValue(node, 'value', opts.functionName, boundStripTransform);
             }
         });
     };
